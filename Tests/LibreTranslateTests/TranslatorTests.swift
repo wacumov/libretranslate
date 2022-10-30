ba3553c2600af2eb18594462a307a94cd84bcc98
Mikhail Akopov <mikhail.akopov.ios@gmail.com>
2022-10-30 17:15:51 +0200

    Add tests

--- Tests/LibreTranslateTests/TranslatorTests.swift
+++ Tests/LibreTranslateTests/TranslatorTests.swift
@@ -1,4 +1,16 @@
 import LibreTranslate
 import XCTest
 
-final class TranslatorTests: XCTestCase {}
+final class TranslatorTests: XCTestCase {
+    let translator = Translator("https://libretranslate.de")
+
+    func testLanguages() async throws {
+        let languages = try await translator.languages()
+        XCTAssertFalse(languages.isEmpty)
+    }
+
+    func testTranslate() async throws {
+        let translation = try await translator.translate("Hello", from: "en", to: "es")
+        XCTAssertFalse(translation.isEmpty)
+    }
+}
